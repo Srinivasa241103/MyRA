8119d22ab0b5f18b82a1176bec88920e7e7a3d8f
M.Srinivsas shankar <cherry@MSrinivsass-MacBook-Air.local>
2026-03-31 04:06:02 +0530

    scroll animation in chats

--- frontend/src/pages/ChatPage.jsx
+++ frontend/src/pages/ChatPage.jsx
@@ -4,7 +4,7 @@ function ChatPage() {
   return (
     <div className="h-full overflow-hidden text-white flex justify-center">
       <div className="w-full max-w-5xl px-6 py-6 flex flex-col h-full overflow-hidden">
-        <div className="flex-1 min-h-0">
+        <div className="flex-1 min-h-0 flex flex-col">
           <ChatWindow />
         </div>
       </div>
