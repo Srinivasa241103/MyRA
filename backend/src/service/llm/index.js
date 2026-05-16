4a66ecf1d9ef3514d6074107e46a18f8e8e66a53
M.Srinivsas shankar <cherry@MSrinivsass-MacBook-Air.local>
2026-05-17 02:08:28 +0530

    cleaning up the code

--- backend/src/service/llm/index.js
+++
@@ -1,6 +0,0 @@
-import promptTemplates from "./promptTemplates.js";
-import geminiService from "./geminiService.js";
-import conversationMemory from "./conversationMemory.js";
-import chatService from "./chatService.js";
-
-export { promptTemplates, geminiService, conversationMemory, chatService };
